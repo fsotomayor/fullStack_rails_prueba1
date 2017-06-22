c657d370bcd1bfdade8ff6dc2286bc793fe3a985
fsotomayor <francisco.sotomayor@hotmail.com>
2017-06-21 21:25:10 -0400

    quinto commit, nuevo controller experiments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,29 @@
 class PagesController < ApplicationController
   def batman
   end
+
+  def superman
+
+  end
+
+  def batman_vs_superman
+  end
+
+  def save_batman
+  	if params[:nombre].present?
+  		VotosBatman.create(name: params[:nombre])
+  	end
+  	if params[:email].present?
+  		VotosBatman.create(name: params[:email])
+  	end
+  end
+
+  def save_superman
+  	if params[:nombre].present?
+  		VotosSuperman.create(name: params[:nombre])
+  	end
+  	if params[:email].present?
+  		VotosSuperman.create(name: params[:email])
+  	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'experiments/page1'
+
+  get 'experiments/page2'
+
+  get 'experiments/page3'
+
+  root 'pages#batman-vs-superman'
+
+  get 'pages/batman-vs-superman'
   get 'pages/batman'
+  get 'pages/superman'
+  post 'pages/save_batman'
+  post 'pages/save_superman'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
